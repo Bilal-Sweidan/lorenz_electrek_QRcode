cf96a960a8bb015e81ffd99ef11f4cbf47623154
Bilal-Sweidan <bilalsweidan58@gmail.com>
2024-12-23 23:04:35 +0300

    2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,21 +7,21 @@ function App() {
       <ul className='' style={{ listStyle: "none", padding: "0" }}>
         <li className='link'>
           <a href="https://2u.pw/q6FbSljF">
-            <img src='../public/rb_136728.png' alt="" />
+            <img src='/rb_136728.png' alt="" />
             instagrame
           </a>
         </li>
 
         <li className='link'>
           <a href="https://2u.pw/anXDE5Ln">
-            <img src='../public/rb_2151296979.png' alt="" />
+            <img src='/rb_2151296979.png' alt="" />
             what's app
           </a>
         </li>
 
         <li className='link'>
           <a href="https://lorenzelektrek.onrender.com" target='_blank'>
-            <img src='../public/150897753_10507204.png' alt="" />
+            <img src='/150897753_10507204.png' alt="" />
             website
           </a>
         </li>
